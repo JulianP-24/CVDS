421eedfeeb2711d7118b28ff1ee62733d458d820
JulianP-24 <88836360+JulianP-24@users.noreply.github.com>
2021-09-02 08:24:41 -0500

    Delete Lab-04 directory

--- Lab-04/src/main/java/hangman/model/BonusScore.java
+++
@@ -1,9 +0,0 @@
-package hangman.model;
-
-public class BonusScore implements GameScore {
-
-    public int CalculateScore(int CorrectCount, int IncorrectCount) throws GameScoreException{
-        return 100;
-    }
-    
-}
