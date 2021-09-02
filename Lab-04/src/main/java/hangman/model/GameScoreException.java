83299807d64573d6289baeffd497e7d14bba070d
JulianP-24 <88836360+JulianP-24@users.noreply.github.com>
2021-09-02 08:26:47 -0500

    Delete Lab-04 directory

--- Lab-04/src/main/java/hangman/model/GameScoreException.java
+++
@@ -1,8 +0,0 @@
-package hangman.model;
-
-public class GameScoreException extends Exception {
-    public static final String INVALID_PARAMETERS = "Los parametros son invalidos";
-    public GameScoreException(String mensaje){
-        super(mensaje);
-    }
-}
--- Lab-04/src/main/java/hangman/model/PowerScore.java
+++
@@ -1,9 +0,0 @@
-package hangman.model;
-
-public class PowerScore implements GameScore {
-    public int CalculateScore(int CorrectCount, int IncorrectCount) throws GameScoreException{
-        return 100;
-    }
-
-    
-}
